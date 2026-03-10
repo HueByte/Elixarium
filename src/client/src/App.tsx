03b812c011d253c0cf6b2fe4ee3612f1d87df1a1
HueByte <ihuebyte@gmail.com>
2026-03-10 04:16:10 +0100

    feat: add mobile notice and responsive book wrapper for better mobile experience

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -6,7 +6,18 @@ export default function App() {
   return (
     <>
       <StarField />
-      <Book />
+      <div className="mobile-notice">
+        <div className="mobile-notice-title">The Elixir Codex</div>
+        <div className="mobile-notice-ornament">{'\u2726 \u2727 \u2726'}</div>
+        <div className="mobile-notice-text">
+          This tome refuses to be read on a device this small.
+          <br />
+          <em>Seek a wider screen, traveller.</em>
+        </div>
+      </div>
+      <div className="book-wrapper">
+        <Book />
+      </div>
     </>
   );
 }
